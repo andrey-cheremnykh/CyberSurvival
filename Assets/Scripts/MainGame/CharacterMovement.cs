63fb4e7c3db1df364c45d80ae04931c3e5fb65d4
Andrey <Andrey@DESKTOP-NI1F48K>
2023-05-05 20:41:39 +0300

    Player Controls

---
+++ Assets/Scripts/MainGame/CharacterMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] float maxSpeed = 6;
+    Rigidbody rb;
+    PhotonView view;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (view.IsMine) 
+        {
+            float inputX = Input.GetAxis("Horizontal") * maxSpeed;
+            float inputZ = Input.GetAxis("Vertical") * maxSpeed;
+            rb.velocity = new Vector3(inputX, 0, inputZ);
+        }
+        
+    }
+}
